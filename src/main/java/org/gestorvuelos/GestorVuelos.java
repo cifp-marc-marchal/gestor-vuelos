19a17eb2f9da34b7f743388700a8ebfc034a9af6
joshuamoniz <joshuaalejandromonizurgiles@a.cifpperedesongall.org>
2025-04-15 13:54:13 +0200

    consulta vuelos por dia y semana, tests

--- src/main/java/org/gestorvuelos/GestorVuelos.java
+++ src/main/java/org/gestorvuelos/GestorVuelos.java
@@ -1,5 +1,7 @@
 package org.gestorvuelos;
 
+import java.time.LocalDate;
+
 public class GestorVuelos {
     private Vuelo[] vuelos = new Vuelo[0];
 
@@ -27,4 +29,64 @@ public class GestorVuelos {
         }
         vuelos = temp;
     }
+
+    public static Vuelo[] consultarVolsPerDia(Vuelo [] vuelos, LocalDate fecha) {
+
+        Vuelo[] temp = new Vuelo[vuelos.length];
+        int contador = 0;
+
+        for (int i = 0; i < vuelos.length; i++) {
+            if (vuelos[i].getFecha().equals(fecha)) {
+                temp[contador] = vuelos[i];
+                contador++;
+            }
+        }
+
+        Vuelo[] resultadoFinal = new Vuelo[contador];
+        for (int i = 0; i < contador; i++) {
+            resultadoFinal[i] = temp[i];
+        }
+
+        return resultadoFinal;
+    }
+
+    public static Vuelo[] consultarVolsPerSetmana(Vuelo[] vuelos, LocalDate fechaInicioSemana) {
+        Vuelo[] temp = new Vuelo[vuelos.length];
+        int contador = 0;
+
+        int diaInicio = fechaInicioSemana.getDayOfMonth();
+        int mesInicio = fechaInicioSemana.getMonthValue();
+        int anioInicio = fechaInicioSemana.getYear();
+
+        for (int i = 0; i < vuelos.length; i++) {
+            LocalDate fechaVuelo = vuelos[i].getFecha();
+
+            int diaVuelo = fechaVuelo.getDayOfMonth();
+            int mesVuelo = fechaVuelo.getMonthValue();
+            int anioVuelo = fechaVuelo.getYear();
+
+            if (anioVuelo == anioInicio && mesVuelo == mesInicio) {
+                if (diaVuelo >= diaInicio && diaVuelo <= diaInicio + 6) {
+                    temp[contador] = vuelos[i];
+                    contador++;
+                }
+            }
+        }
+
+        Vuelo[] resultadoFinal = new Vuelo[contador];
+        for (int i = 0; i < contador; i++) {
+            resultadoFinal[i] = temp[i];
+        }
+
+        return resultadoFinal;
+    }
+
+
+
+
 }
+
+
+
+
+
